d41e0d853b093f2ec5c521eea8b27f3aad7afc4b
Bryson Sutton <139702261+BSutton17@users.noreply.github.com>
2023-08-06 00:39:54 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,10 @@ function App() {
   const [originalValue, setOriginalValue] = useState(0);
   const [measurement, setMeasurement] = useState('feet');
   const [converted, setConverted] = useState('meters');
-
+  
   const converter = (e: { target: { value: string; }; }) => {
     const inputValue = parseFloat(e.target.value);
-
+    
     if (isNaN(inputValue)) {
       // Handle invalid input (non-numeric)
       setValue(0);
@@ -24,10 +24,10 @@ function App() {
         baseValue = inputValue;
         break;
       case 'meters':
-        baseValue = inputValue * 3.28084;
+        baseValue = inputValue / 0.3048;
         break;
       case 'yards':
-        baseValue = inputValue / 3;
+        baseValue = inputValue * 3;
         break;
       case 'centimeters':
         baseValue = inputValue / 30.48;
@@ -49,10 +49,10 @@ function App() {
         result = baseValue;
         break;
       case 'meters':
-        result = baseValue / 3.28084;
+        result = baseValue * 0.3048;
         break;
       case 'yards':
-        result = baseValue * 3;
+        result = baseValue / 3;
         break;
       case 'centimeters':
         result = baseValue * 30.48;
@@ -71,12 +71,12 @@ function App() {
 
   return (
     <>
-    <h1 id="title">Measurement Converter</h1>
+      <h1 id="title">Measurement Converter</h1>
       <div id="wrapper">
         <h2>Convert {measurement} to {converted}</h2>
         <div id="container">
           <input id="value" type="number" required onChange={converter} />
-          <div id="select">
+          <div id="select" >
             <select id="measurement" value={measurement} onChange={(e) => setMeasurement(e.target.value)}>
               <option value="feet">Feet</option>
               <option value="meters">Meters</option>
@@ -86,8 +86,8 @@ function App() {
               <option value="kilometers">Kilometers</option>
             </select>
           </div>
-          <div id="converted">
-            <select id="second" value={converted} onChange={(e) => setConverted(e.target.value)}>
+          <div id="select">
+            <select id="converted" value={converted} onChange={(e) => setConverted(e.target.value)}>
               <option value="feet">Feet</option>
               <option value="meters">Meters</option>
               <option value="yards">Yards</option>
